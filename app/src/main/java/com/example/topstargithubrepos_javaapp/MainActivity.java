efd0e7ac66e06bc37ed07f05e1c1e950d44fa7cf
jzelic00 <jzelic00@fesb.hr>
2020-11-03 22:04:09 +0100

    Dodavanje volley-a i parsiranja podataka

--- app/src/main/java/com/example/topstargithubrepos_javaapp/MainActivity.java
+++ app/src/main/java/com/example/topstargithubrepos_javaapp/MainActivity.java
@@ -5,9 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -21,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
 
+        final ArrayList<Model> GithubRepos=new ArrayList<>();
         String url = "https://api.github.com/search/repositories?q=stars:%3E=100000";
+
+
+
 
         final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -33,7 +49,37 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add(demo2);
 
 
+
         final RecyclerView.Adapter adapter = new MyAdapter(arrayList);
         recyclerView.setAdapter(adapter);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Gson gson = new Gson();
+                try {
+
+                    JSONArray jsonArray = response.getJSONArray("items");
+                    Log.d("a", "poruka");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                        Model test = gson.fromJson(String.valueOf(jsonObject), Model.class);
+                        GithubRepos.add(test);
+
+                    }
+                    Log.d("jeee", "evo");
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Log.d("Prvi error", "lose");
+                }
+            }
+            }, error -> {
+                error.printStackTrace();
+                Log.d("Zadnji error", error.getMessage());
+        }
+        );
     }
 }
+
